5c157d2604d77c81b230f415fa917c3ff83db2de
Gregory Hosono <ghosono@gmail.com>
2013-12-22 00:40:20 -0800

    Updated from Dropbox

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var $win = $(window);
+
+function slowScroll($elem) {
+  var startOffset = 180;
+  var scrollDistance = 1080; // or 1080 - $elem.height();
+  
+  scrollTimer = null;
+  var scrollTop = $win.scrollTop();
+  if (scrollTop < scrollDistance) {
+    // keeps the percentage of distance traveled consistent
+    var topOffset = scrollTop + ((scrollTop + startOffset) / scrollDistance) * (scrollDistance - scrollTop - startOffset);
+    $elem.css('top', topOffset + 'px');
+  }
+};
+
+var scrollTimer = null;
+$(window).scroll(function() {
+  if (scrollTimer) {
+    clearTimeout(scrollTimer);   // clear any previous pending timer
+  }
+  scrollTimer = setTimeout(slowScroll($('#header-text')), 500);   // set new timer
+});
