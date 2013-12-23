08a2618f0105a083c1d94b128b1766ba75575c46
Alex Jiang <dashuai99@gmail.com>
2013-12-23 00:35:22 -0600

    add google analytics highchart and corresponding js also cleaned up comments in js/scripts.js and index.html

--- js/charts.js
+++ js/charts.js
@@ -36,10 +36,6 @@ $(function () {
         cursor: 'pointer',
         dataLabels: {
           enabled: false
-          // enabled: true,
-          // color: '#000000',
-          // connectorColor: '#000000',
-          // format: '<b>{point.name}</b>: {point.percentage:.1f}%'
         }
       }
     },
@@ -53,5 +49,52 @@ $(function () {
         ['Off-campus opportunities', 160000]
       ]
     }]
+  });
+
+  $('#analytics-graph').highcharts({
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false
+    },
+    credits: {
+      enabled: false
+    },
+    legend: {
+      enabled: false
+    },
+    navigation: {
+      buttonOptions: {
+        enabled: false
+      }
+    },
+    title: {
+      text: 'Monthly Unique Visitors in 2013',
+      style: {
+        color: '#000000',
+        fontSize: '2.0rem'
+      }
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.y}</b>'
+    },
+    series: [{
+      type: 'line',
+      name: 'Unique Visitors',
+      data: [7580, 6744, 7930, 9839, 6717, 8296, 3109, 3264, 6221, 10526, 8892, 4738],
+      pointStart: Date.UTC(2013, 0, 1),
+      pointInterval: 1000 * 3600 * 24 * 31 // one month in milliseconds
+    }],
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: {
+        month: '%b \'%y'
+      }
+    },
+    yAxis: {
+      title: {
+        text: ''
+      }
+    }
   });
 });
