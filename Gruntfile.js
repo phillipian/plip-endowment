460d7b0aba90ce0bf0326c3ca38dff84ae71d1e7
Alex Jiang <dashuai99@gmail.com>
2014-01-14 00:38:32 -0500

    implement grunt.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,74 @@
 module.exports = function(grunt) {
-  // Do grunt-related things in here
-  
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        report: 'min',
+        mangle: true
       },
       build: {
-        src: 'js/*.js',
-        dest: 'js/endowment.min.js'
+        files: [{
+          src: ['js/*.js', '!js/endowment.min.js'],
+          dest: 'js/endowment.min.js'
+        }]
+      }
+    },
+    cssmin: {
+      options: {
+        report: 'min'
+      },
+      minify: {
+        files: [{
+          src: ['css/*.css', '!css/*.min.css'],
+          dest: 'css/endowment.min.css'
+        }]
+      }
+    },
+    imagemin: {
+      dynamic: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [ {
+          expand: true,
+          cwd: 'images/',
+          src: '*.jpg',
+          dest: 'public/assets'
+        } ]
+      }
+      
+    },
+    versioning: { // Task
+      options: { // Task options
+        cwd: 'public',
+        outputConfigDir: ''
+      },
+      dist: { // Target
+        options: { // Target options
+        },
+        files: [ {
+          assets: '<%= uglify.build.files %>',
+          key: 'global',
+          dest: 'assets/',
+          type: 'js',
+          ext: '.min.js'
+        }, {
+          assets: '<%= cssmin.minify.files %>',
+          key: 'global',
+          dest: 'assets/',
+          type: 'css',
+          ext: '.min.css'
+        } ]
       }
     }
   });
   
   // Load plugins.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  // grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-static-versioning');
   
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', [ 'uglify', 'cssmin', 'imagemin', 'versioning' ]);
 };
